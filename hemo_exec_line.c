adcc944877b5b46afcb91c699e0315575072d98c
Hemo5478 <hamza9638@hotmail.com>
2023-04-27 22:21:04 -0700

    Fixing

---
+++ hemo_exec_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *hemo_exec_line - a function that execute the line
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void hemo_exec_line(char *line)
+{
+	pid_t child;
+	int status;
+	char *myargs[] = {NULL};
+	char *myenv[] = {NULL};
+
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(line, myargs, myenv))
+		{
+			perror("hsh");
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (child > 0)
+	{
+		wait(&status);
+	}
+}
